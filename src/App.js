d5955f8e09115013cb1b6dad4487b9c8e010b99c
Jokzin <jookz1@gmail.com>
2020-04-11 15:34:33 +0200

    Add component sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header/Header'
+import Header from './components/Header/Header';
+import Sidebar from './components/Sidebar/Sidebar';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Sidebar />
     </div>
   );
 }
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Sidebar(){
+
+    return (
+        <div className="sidebar">
+            <h2 className="titreSidebar">Chaînes recommandées</h2>
+            <ul className="listeStream">
+
+            </ul>
+        </div>
+    )
+}
+
+export default Sidebar;

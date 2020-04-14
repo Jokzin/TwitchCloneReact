f1ee560edafb8bc575bc6ee2aee1463e75e9cf8b
Jokzin <jookz1@gmail.com>
2020-04-14 15:54:18 +0200

    Retrieve games and users info for recommended stream

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,6 +1,54 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import api from '../../api';
 
 function Sidebar(){
+
+    const apiURL = process.env.REACT_APP_API_URL;
+    const [topStreams, setTopStreams] = useState([]);
+
+    useEffect(() => {
+        const fecthData = async() => {
+            const result = await api.get(apiURL + '/streams');
+
+            let dataArray = result.data.data;
+            let gameIDs = dataArray.map(stream => {
+                return stream.game_id;
+            })
+            let userIDs = dataArray.map(stream => {
+                return stream.user_id;
+            })
+
+            //création url personnalisés
+            let baseUrlGames = apiURL + '/games?';
+            let baseUrlUsers = apiURL + '/users?';
+
+            let queryParamsGames = '';
+            let queryParamsUsers = '';
+
+            gameIDs.map(id => {
+                return (queryParamsGames = queryParamsGames + `id=${id}&`);
+            })
+
+            userIDs.map(id => {
+                return (queryParamsUsers = queryParamsUsers + `id=${id}&`);
+            })
+
+            // url finale
+            let urlFinalGames = baseUrlGames + queryParamsGames;
+            let urlFinalUsers = baseUrlUsers + queryParamsUsers;
+
+            // appel
+            let gamesNames = await api.get(urlFinalGames);
+            let getUsers = await api.get(urlFinalUsers);
+
+            let gamesNamesArray = gamesNames.data.data;
+            let arrayUsers = getUsers.data.data;
+
+
+        }
+
+        fecthData();
+    }, [])
 
     return (
         <div className="sidebar">

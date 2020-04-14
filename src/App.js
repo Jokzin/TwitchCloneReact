880e725a31472c9ed04ae27d151179c7e512a840
Jokzin <jookz1@gmail.com>
2020-04-14 14:00:24 +0200

    Add games component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
+import Games from './components/Games/Games';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Header />
       <Sidebar />
+      <Games />
     </div>
   );
 }

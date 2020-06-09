b1ad338678a962270d92ed2236631aa854cbec51
Jokzin <jookz1@gmail.com>
2020-06-09 13:32:51 +0200

    add OAuth due to twitch API change

--- src/api.js
+++ src/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 let api = axios.create({
     headers: {
-        'Client-ID': process.env.REACT_APP_API_KEY
+        'Client-ID': process.env.REACT_APP_API_KEY,
+        'Authorization': 'Bearer ' + process.env.REACT_APP_ACCESS_TOKEN
     }
 })
 

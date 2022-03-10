092aeda41a72e7ffa73d54fc50d9dbb9ef44cbf3
Halfkan Julien <jookz1@gmail.com>
2022-03-11 00:40:04 +0100

    Menu bar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,35 +1,53 @@
-import React from 'react';
-import logo from './IconeTwitch.svg';
-import search from './Search.svg';
-import menuIco from './MenuIco.svg';
+import React from 'react'
+import logo from './IconeTwitch.svg'
+import search from './Search.svg'
+import menuIco from './MenuIco.svg'
+import { Link } from 'react-router-dom'
 
-function Header(){
-
+function Header() {
     return (
         <div>
             <nav className="headerTop">
                 <ul className="listMenu">
                     <li className="liensNav">
-                        <img src={logo} alt="logo twitch" className="logo"/>
+                        <Link className="lien" to="/">
+                            <img
+                                src={logo}
+                                alt="logo twitch"
+                                className="logo"
+                            />
+                        </Link>
                     </li>
                     <li className="liensNav">
-                        Top Games
+                        <Link className="lien" to="/">
+                            Top Games
+                        </Link>
                     </li>
                     <li className="liensNav">
-                        Top Streams
+                        <Link className="lien" to="/top-streams">
+                            Top Streams
+                        </Link>
                     </li>
                     <li className="liensNav">
                         <form className="formSubmit">
-                            <input type="text" className="inputRecherche"/>
+                            <input type="text" className="inputRecherche" />
                             <button type="submit">
-                                <img src={search} alt="icone loupe" className="logoLoupe"/>
+                                <img
+                                    src={search}
+                                    alt="icone loupe"
+                                    className="logoLoupe"
+                                />
                             </button>
                         </form>
                     </li>
                 </ul>
             </nav>
             <div className="menuResBtn">
-                <img src={menuIco} alt="icone menu responsive" className="menuIco"/>
+                <img
+                    src={menuIco}
+                    alt="icone menu responsive"
+                    className="menuIco"
+                />
             </div>
         </div>
     )
